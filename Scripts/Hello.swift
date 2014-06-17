901cbd264381d7eccfedc5f80ac5eff410ec6c0e
Bennett Smith <bennett@focalshift.com>
2014-06-17 11:43:08 -0700

    Adds command-line script.

---
+++ Scripts/Hello.swift
@@ -0,0 +1,12 @@
+#!/usr/bin/env xcrun swift -i
+
+// Be sure to select Xcode6-Beta using xcode-select.
+//
+// $ xcode-select -switch /Applications/Xcode6-Beta.app
+//
+// Also be sure to set the execute bit on your script!
+//
+// $ chmod a+x Hello.swift
+
+println("Hello, Swift!")
+
